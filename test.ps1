93456076e346d47df531d4ab7cdb7447a87be6f3
Kundan9 <kundan579p@hotmail.com>
2018-10-20 16:16:12 +0530

    Lecture 2

---
+++ test.ps1
@@ -0,0 +1,24 @@
+﻿$%New-Item -ItemType Directory c:\Kundan
+New-Item -ItemType File C:\kundan\test.txt
+Set-Content -Value "Test" -Path "C:\Kundan\test.txt" 
+Get-Content -Path "C:\Kundan\test.txt" | Out-File -FilePath "C:\kundan\test.txt" -Append
+"Test1" | Out-File "C:\Kundan\test.txt" -Append
+
+Get-Item -Path "Env:\Path" | add-c
+
+Get-EventLog -LogName Application | select source 
+Write-EventLog -ComputerName NTMS_PC10 -LogName Application -Source chrome -EventId 1004 -Message "My name is Kundan"
+
+$myvar="This is test" 
+$myvar | Get-Member
+$myvar.ToUpper()
+$myvar.Length
+$myvar.Substring(0,4)
+$myvar.Contains('is')
+$myvar.AddDays(4)
+$myvar | Get-Date -UFormat "%Y-%m-%d"
+
+$service= Get-Service
+$file= $service | Select name,statu
+$file | Out-File -FilePath "c:\Kundan\service-$myvar.txt"
+
